7b49368eddef841c029a961b119069d50c8ff0d2
Nathan Toups <n@rjrbt.io>
2022-12-03 16:11:20 -0700

    feat: day 1 part 1 and 2 solutions

---
+++ day_01/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+// This is a quick and dirty solution for part 1 and 2. I created a map of elves
+// and exported a slice of the totals, and then reverse sorted the list to solve
+// part 1 and 2
+
+func elfMapFromFile(name string) (map[int]int, error) {
+	elfMap := make(map[int]int)
+	f, err := os.Open(name)
+	defer f.Close()
+	if err != nil {
+		return elfMap, err
+	}
+	scanner := bufio.NewScanner(f)
+	currentElf := 1
+
+	for scanner.Scan() {
+		newLine := scanner.Text()
+		if newLine == "" {
+			currentElf++
+		} else {
+			v, err := strconv.Atoi(newLine)
+			if err != nil {
+				return elfMap, err
+			}
+			elfMap[currentElf] += v
+		}
+	}
+	return elfMap, nil
+}
+
+func main() {
+	elfMap, err := elfMapFromFile("elf_cals.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var t []int
+
+	for _, v := range elfMap {
+		t = append(t, v)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(t)))
+
+	fmt.Println("Answer Part 1:", t[0])
+	fmt.Println("Answer Part 2:", t[0]+t[1]+t[2])
+
+}

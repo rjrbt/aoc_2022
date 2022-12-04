b33906628a80298a780a5f7727cc6089152e58de
Nathan Toups <n@rjrbt.io>
2022-12-03 22:42:59 -0700

    feat: completed day 4

---
+++ day_04/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type set map[int]struct{}
+
+func (s set) add(x int) {
+	s[x] = struct{}{}
+}
+
+// contains compares a set to another set and returns true is a is contained by the set
+// otherwise it returns false
+func (s set) contains(a set) bool {
+	for k := range s {
+		if _, ok := a[k]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func (s set) intersect(a set) bool {
+	for k := range s {
+		if _, ok := a[k]; ok {
+			return true
+		}
+	}
+	return false
+}
+
+// genSet takes a number range string such as "22-88" and returns a map of keys from 22 to 88
+func genSet(s string) set {
+	m := make(set)
+	r := strings.Split(s, "-")
+	start, _ := strconv.Atoi(r[0])
+	end, _ := strconv.Atoi(r[1])
+	for i := start; i <= end; i++ {
+		m.add(i)
+	}
+	return m
+}
+
+func main() {
+	f, _ := os.Open("input.txt")
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	matchCounter := 0
+	overlapCounter := 0
+
+	for scanner.Scan() {
+		newLine := scanner.Text()
+		setStrings := strings.Split(newLine, ",")
+		a, b := genSet(setStrings[0]), genSet(setStrings[1])
+
+		if a.contains(b) {
+			matchCounter++
+		} else if b.contains(a) {
+			matchCounter++
+		}
+		if a.intersect(b) {
+			overlapCounter++
+		}
+	}
+	fmt.Println("contained:", matchCounter)
+	fmt.Println("overlap:", overlapCounter)
+}
